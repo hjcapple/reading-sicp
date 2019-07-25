12b1f226093468856f93afc934b93f40a06007b8
HJC <hjcapple@gmail.com>
2019-07-25 22:30:06 +0800

    添加  采用牛顿法求平方根、练习 1.6

--- chapter_1/README.md
+++ chapter_1/README.md
@@ -5,4 +5,6 @@
 * P13 - [练习 1.3](./exercise_1_3.lua)
 * P13 - [练习 1.4](./exercise_1_4.md)
 * P14 - [练习 1.5](./exercise_1_5.md)
+* P15 - [实例： 采用牛顿法求平方根](./newton_sqrt.lua)
+* P16 - [练习 1.6](./exercise_1_6.md)
 
---
+++ chapter_1/newton_sqrt.lua
@@ -0,0 +1,42 @@
+-- P15 - 采用牛顿法求平方根
+
+function average(x, y)
+	return (x + y) / 2
+end
+
+function abs(x)
+	if x < 0 then 
+		return -x
+	else
+		return x
+	end
+end
+
+function square(x)
+	return x * x
+end
+
+function sqrt_iter(guess, x)
+	if good_enough(guess, x) then 
+		return guess
+	else 
+		return sqrt_iter(improve(guess, x), x)
+	end
+end
+
+function improve(guess, x)
+	return average(guess, x / guess)
+end
+
+function good_enough(guess, x)
+	return abs(square(guess) - x) < 0.001
+end
+
+function sqrt(x)
+	return sqrt_iter(1.0, x)
+end
+
+print(sqrt(9))
+print(sqrt(100 + 37))
+print(sqrt(sqrt(2) + sqrt(3)))
+print(square(sqrt(1000)))
