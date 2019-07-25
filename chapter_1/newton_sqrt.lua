0fabd2bf0007e3fdb400cc6dbc8ad81fda37f8fc
HJC <hjcapple@gmail.com>
2019-07-25 22:33:13 +0800

    newton_sqrt.lua 中的 Tab 转换成空格

--- chapter_1/newton_sqrt.lua
+++ chapter_1/newton_sqrt.lua
@@ -1,39 +1,39 @@
 -- P15 - 采用牛顿法求平方根
 
 function average(x, y)
-	return (x + y) / 2
+    return (x + y) / 2
 end
 
 function abs(x)
-	if x < 0 then 
-		return -x
-	else
-		return x
-	end
+    if x < 0 then 
+        return -x
+    else
+        return x
+    end
 end
 
 function square(x)
-	return x * x
+    return x * x
 end
 
 function sqrt_iter(guess, x)
-	if good_enough(guess, x) then 
-		return guess
-	else 
-		return sqrt_iter(improve(guess, x), x)
-	end
+    if good_enough(guess, x) then 
+        return guess
+    else 
+        return sqrt_iter(improve(guess, x), x)
+    end
 end
 
 function improve(guess, x)
-	return average(guess, x / guess)
+    return average(guess, x / guess)
 end
 
 function good_enough(guess, x)
-	return abs(square(guess) - x) < 0.001
+    return abs(square(guess) - x) < 0.001
 end
 
 function sqrt(x)
-	return sqrt_iter(1.0, x)
+    return sqrt_iter(1.0, x)
 end
 
 print(sqrt(9))
