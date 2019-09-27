13a57260562a306c0d0805b957d29a4841c4e19a
HJC <hjcapple@gmail.com>
2019-09-27 21:46:24 +0800

    [练习 3.37]

---
+++ chapter_3/exercise_3_37.scm
@@ -0,0 +1,42 @@
+#lang racket
+
+;; P205 - [练习 3.37]
+
+(#%require "constraints.scm")
+
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
+(define (c* x y)
+  (let ((z (make-connector)))
+    (multiplier x y z)
+    z))
+
+(define (c/ x y)
+  (let ((z (make-connector)))
+    (multiplier z y x)
+    z))
+
+(define (cv x)
+  (let ((z (make-connector)))
+    (constant x z)
+    z))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (celsius-fahrenheit-converter x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+          x)
+      (cv 32)))
+
+(define C (make-connector))
+(define F (celsius-fahrenheit-converter C))
+
+(probe "Celsius temp" C)
+(probe "Fahrenheit temp" F)
+(set-value! C 25 'user)
+
+(forget-value! C 'user)
+(set-value! F 212 'user)
+
