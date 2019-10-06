d3010d95d53b84a71dca4f6471a99d6a26867184
HJC <hjcapple@gmail.com>
2019-10-06 23:10:01 +0800

    [3.5.1 流作为延时的表]

---
+++ chapter_3/ch3support.scm
@@ -0,0 +1,21 @@
+#lang racket
+
+(provide prime?)
+
+;; P33 - 1.2.6 实例： 素数检测, [寻找因子]
+(define (square x) (* x x))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
---
+++ chapter_3/stream.scm
@@ -0,0 +1,55 @@
+#lang sicp
+
+;; P220 - [3.5.1 流作为延时的表]
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (cond ((not (stream-null? s))
+         (proc (stream-car s))
+         (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (display x)
+  (newline))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream low
+                   (stream-enumerate-interval (+ low 1) high))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(#%require "ch3support.scm")
+
+(stream-car
+  (stream-cdr
+    (stream-filter prime?
+                   (stream-enumerate-interval 10000 1000000))))
+
+(stream-ref (stream-filter prime?
+                           (stream-enumerate-interval 10000 1000000)) 1)
+
