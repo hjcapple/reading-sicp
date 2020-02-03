3703153d390dd8945f821e998d4f35910e5a38c4
HJC <hjcapple@gmail.com>
2020-02-03 16:07:12 +0800

    [练习 5.41]

---
+++ chapter_5/exercise_5_41.scm
@@ -0,0 +1,27 @@
+#lang sicp
+
+;; P424 - [练习 5.41]
+
+(define (find-variable var env)
+  (define (position-in-frame var frame position)
+    (if (null? frame)
+        'not-found
+        (if (eq? var (car frame))
+            position
+            (position-in-frame var (cdr frame) (+ position 1)))))
+  
+  (define (loop var env offset)
+    (if (null? env)
+        'not-found
+        (let ((position (position-in-frame var (car env) 0)))
+          (if (not (eq? position 'not-found))
+              (list offset position)
+              (loop var (cdr env) (+ offset 1))))))
+  
+  (loop var env 0))
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+(find-variable 'c '((y z) (a b c d e) (x y))) ; (1 2)
+(find-variable 'x '((y z) (a b c d e) (x y))) ; (2 0)
+(find-variable 'w '((y z) (a b c d e) (x y))) ; not-found
+
